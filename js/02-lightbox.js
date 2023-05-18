1e2527f9b644484edf5d52e4e4e1fd3d348e83f5
Daria Shpak <darya.shpak@gmail.com>
2023-05-18 14:47:14 +0300

    Task 2 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery")
+
+  ; (function () {
+    const markupGallery = galleryItems
+      .map(
+        ({ preview, original, description }) =>
+          `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" 
+     alt="${description}"/>
+   </a>
+  </li>`
+      )
+      .join("")
+
+    gallery.insertAdjacentHTML("beforeend", markupGallery)
+  })();
+
+
+let lightbox = new SimpleLightbox(".gallery li a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  scrollZoom: false,
+})
+
+
